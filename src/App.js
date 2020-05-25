c7c1d65b1c07ff4eaef8707342b91361ae3b2e36
Joe Thompson <jthompso@mtu.edu>
2020-05-24 19:37:48 -0700

    Created static table content

--- src/App.js
+++ src/App.js
@@ -1,26 +1,117 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import './App.css'
 
-function App() {
+function App () {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <BookTable />
+  )
 }
 
-export default App;
+class BookTable extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      books: [
+        {
+          title: 'Yurts: Living in the Round',
+          author: 'Becky Kemery',
+          pages: 146,
+          read: false
+        },
+        {
+          title: 'Psychovertical',
+          author: 'Andy Kirkpatrick',
+          pages: 288,
+          read: true
+        },
+        {
+          title: 'Dead Mountain: The Untold True Story of the Dyatlov Pass Incident',
+          author: 'Donnie Eichar',
+          pages: 288,
+          read: true
+        },
+        {
+          title: 'Reinhold Messner: My Life At The Limit',
+          author: 'Reinhold Messner',
+          pages: 256,
+          read: false
+        }
+      ]
+    }
+  }
+
+  render() {
+    return (
+      <table>
+        <tr>
+          <td></td>
+          <th>Title</th>
+          <th>Author</th>
+          <th>Pages</th>
+          <th>Read?</th>
+        </tr>
+        {this.state.books.map((book) => {
+          return <BookRow 
+            title = {book.title}
+            author = {book.author}
+            pages = {book.pages}
+            read = {book.read ? 'yes' : 'no'}
+          />
+        })}
+        <AddBookForm />
+      </table>
+    )
+  }
+}
+
+function BookRow (props) {
+  return (
+    <tr>
+      <td>
+        <DeleteBookButton />
+      </td>
+      <td>{props.title}</td>
+      <td>{props.author}</td>
+      <td>{props.pages}</td>
+      <td>{props.read}</td>
+    </tr>
+  )
+}
+
+class AddBookForm extends React.Component {
+  render() {
+    return (
+      <tr>
+        <td>
+          <AddBookButton />
+        </td>
+        <td>
+          <input type='text' placeholder='Enter book details...' required></input>
+        </td>
+        <td>
+          <input type='text' required></input>
+        </td>
+        <td>
+          <input type='number' required></input>
+        </td>
+        <td>
+          <input type='checkbox'></input>
+        </td>
+      </tr>
+    )
+  }
+}
+
+function DeleteBookButton () {
+  return (
+    <button>X</button>
+  )
+}
+
+function AddBookButton () {
+  return (
+    <button>+</button>
+  )
+}
+
+export default App
